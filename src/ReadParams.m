66316e9c8a6e5234fbac44c0664946b1d49fd1bb
stevetuozzolo <stephen.tuozzolo@gmail.com>
2016-09-01 13:45:09 -0400

    Added optional line to param file for time step
    New script looks for 2 extra lines in param file that show the time step (as a fraction of days). If there is no such line, it defaults to 1.
    
    Exp.tUse creates a vector of sequential times based on the aforementioned timestep. This allows for using input files with timesteps smaller than 1 day.

--- src/ReadParams.m
+++ src/ReadParams.m
@@ -4,13 +4,22 @@ fid=fopen(fname,'r');
 fgetl(fid); Chain.N=fscanf(fid,'%f',1); fscanf(fid,'\n');
 fgetl(fid); Chain.Nburn=fscanf(fid,'%f',1); fscanf(fid,'\n');
 fgetl(fid); R.Seed=fscanf(fid,'%f',1); fscanf(fid,'\n');
-
 fgetl(fid); tUse1=fscanf(fid,'%f',1); tUseEnd=fscanf(fid,'%f',1); fscanf(fid,'\n');
-Exp.tUse=tUse1:tUseEnd;
-Exp.Est_nt=length(Exp.tUse);
-
 fgetl(fid); Prior.meanQbar=fscanf(fid,'%f',1); fscanf(fid,'\n');
 fgetl(fid); Prior.covQbar=fscanf(fid,'%f',1); fscanf(fid,'\n');
+
+if fgetl(fid)~=-1 %if extra line in param file for time step size
+    Exp.tStep=fscanf(fid,'%f',1); fscanf(fid,'\n'); %time step as fraction of day
+else
+    Exp.tStep=1; %default to one day
+end
+fclose(fid);
+
+Exp.tUse=(round(tUse1/Exp.tStep):1:round(tUseEnd/Exp.tStep))*Exp.tStep;
+Exp.Est_nt=length(Exp.tUse);
+
+
+
 fclose(fid);
 
 return
