45fc0a5124513387e96a9646341cda12462f153b
mikedurand <durand.8@osu.edu>
2017-09-26 15:04:56 -0400

    Quasi-Physical Effective n
    Added a “case 5” to the calcnhat to handle the quasi-physical roughness
    coefficient parameterization.

--- src/calcnhat.m
+++ src/calcnhat.m
@@ -8,7 +8,16 @@ switch BjerklienOpt
     case 3
         nhat=na .* (h-hmin+.1).^x1; %relative stage
     case 4
-       nhat=na .* (A./w).^x1; %hydraulic radius
+       nhat=na .* (A./w).^x1; %hydraulic depth power law
+    case 5        
+        %note: using x1=sigmad for now. see notes from july 17, little red notebook
+        %would be better to have prior on average depth & average depth
+        %variation from M&T
+        as=1/2;
+        ad=5/3;        
+        Cd=x1./(A./w);
+        Cs2=(Cd.^2+1) .^( (-ad/as).^2 )-1;  %this can get unrealistic
+        nhat = na .* (1 + 1/2.* (as./(ad+as).*Cd.^2 + ad./(ad+as).*Cs2 ) );
 end
 
 return
