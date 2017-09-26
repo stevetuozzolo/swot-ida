5b04abd9ebdb1eda4501e0703745a3cac710d32f
stevetuozzolo <stephen.tuozzolo@gmail.com>
2017-09-26 16:22:43 -0400

    Put ReadParams back where it belongs!
    Undo a dumb rewriting of readparams... and fix so that it's nice and simple once again.

--- src/ReadParams.m
+++ src/ReadParams.m
@@ -4,23 +4,19 @@ fid=fopen(fname,'r');
 fgetl(fid); Chain.N=fscanf(fid,'%f',1); fscanf(fid,'\n');
 fgetl(fid); Chain.Nburn=fscanf(fid,'%f',1); fscanf(fid,'\n');
 fgetl(fid); R.Seed=fscanf(fid,'%f',1); fscanf(fid,'\n');
-fgetl(fid); Exp.Est_nt=fscanf(fid,'%f',1); fscanf(fid,'\n');
 fgetl(fid); tUse1=fscanf(fid,'%f',1); tUseEnd=fscanf(fid,'%f',1); fscanf(fid,'\n');
 fgetl(fid); Prior.meanQbar=fscanf(fid,'%f',1); fscanf(fid,'\n');
 fgetl(fid); Prior.covQbar=fscanf(fid,'%f',1); fscanf(fid,'\n');
 fgetl(fid); Exp.nOpt=fscanf(fid,'%f',1); fscanf(fid,'\n');
-
-if isempty(Exp.nOpt)
-    Exp.nOpt=3;
-end
 
 if fgetl(fid)~=-1 %if extra line in param file for time step size
     Exp.tStep=fscanf(fid,'%f',1); fscanf(fid,'\n'); %time step as fraction of day
 else
     Exp.tStep=1; %default to one day
 end
+
 Exp.tUse=(round(tUse1/Exp.tStep):1:round(tUseEnd/Exp.tStep))*Exp.tStep;
-%Exp.Est_nt=length(Exp.tUse); %move this up to be in params.txt
+Exp.Est_nt=length(Exp.tUse);
 
 fclose(fid);
 
